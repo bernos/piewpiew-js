ed05a54fb02f682b8f71a8e186cccda5e43bc9ea
Brendan McMahon <bernos@gmail.com>
2012-04-12 22:46:43 +1000

    Moved requirejs configuration block from bootstrap into the actual config file

--- examples/requirejs/js/bootstrap.js
+++ examples/requirejs/js/bootstrap.js
@@ -2,35 +2,17 @@
 
   bootstrap.js
 
-  Gets our app up and running. Generally you won't need to edit this file, 
-  except if you need to make changes to the configuration of the require.js
-  AMD library.
-
 *******************************************************************************/
 
 /**
- * Let require.js know where our libraries live
+ * Load the config, then the main app file, and away we go
  */
-require.config({
-  paths: {
-    // Common libraries
-    'amd-loader': 'libs/amd-loader', // Wrapper for loading non-amd libs
-    'jquery'    : 'libs/jquery/jquery-1.7.1.min',
-    'underscore': 'libs/underscore/underscore',
-    'backbone'  : 'libs/backbone/backbone',
-    'piewpiew'  : 'libs/piewpiew/piewpiew-0.0.1.min',
+require(['config/config'], function(config) 
+{
+  if (config.requirejs) require.config(config.requirejs);
 
-    // Require JS plugins
-    'text'      : 'libs/requirejs/text',
-    'order'     : 'libs/requirejs/order',
-    'i18n'      : 'libs/requirejs/i18n',
-    'domReady'  : 'libs/requirejs/domReady'
-  }
-});
-
-/**
- * Load the main application module and kick things off
- */
-require(['config/config', 'app/main'], function(config, main) {
-  main(config);
+  require(['app/main'], function(main) 
+  {
+    main(config);
+  });  
 });
--- examples/requirejs/js/config/config.js
+++ examples/requirejs/js/config/config.js
@@ -2,6 +2,25 @@ define(function() {
 	return {
 		version: "0.0.1",
 
+    environment: "development",
+
+    requirejs: {
+      paths: {
+        // Common libraries
+        'amd-loader': 'libs/amd-loader', // Wrapper for loading non-amd libs
+        'jquery'    : 'libs/jquery/jquery-1.7.1.min',
+        'underscore': 'libs/underscore/underscore',
+        'backbone'  : 'libs/backbone/backbone',
+        'piewpiew'  : 'libs/piewpiew/piewpiew-0.0.1.min',
+
+        // Require JS plugins
+        'text'      : 'libs/requirejs/text',
+        'order'     : 'libs/requirejs/order',
+        'i18n'      : 'libs/requirejs/i18n',
+        'domReady'  : 'libs/requirejs/domReady'
+      }
+    },
+
     development: {
 
     },
