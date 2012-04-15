245fae6595c5e0089a61052eb39e4df6fa332897
Brendan McMahon <bernos@gmail.com>
2012-04-15 11:21:06 +1000

    Altered requirejs config setup. Can have different requirejs config for each environment now

--- examples/requirejs/js/bootstrap.js
+++ examples/requirejs/js/bootstrap.js
@@ -9,7 +9,54 @@
  */
 require(['config/config'], function(config) 
 {
-  if (config.requirejs) require.config(config.requirejs);
+  /**
+   * Locate the requirejs config block for the current environment in the config 
+   * file, if one is provided, otherwise we'll provide a sensible default
+   */
+  var requireConfig = null;
+  var env   = config.environment != null ? config.environment : "development";
+
+  // First check currently configured environment
+  if (config[env]) {
+    requireConfig = config[env]['requirejs'];
+  }
+
+  // If we didnt find a value, escalate
+  while (null == requireConfig && env != 'production') {
+    switch (env) {
+      case 'development' :
+        env = 'testing';
+        break;
+
+      case 'testing' :
+        env = 'production';
+        break;
+    }
+
+    if(null != config[env]) {
+      requireConfig = config[env]['requirejs'];
+    }        
+  }
+
+  if (requireConfig == null) {
+    console.info('Did not find requirejs config in config file');
+    requireConfig = {
+      paths: {
+        // Common libraries
+        'underscore': 'libs/underscore/underscore',
+        'backbone'  : 'libs/backbone/backbone',
+        'piewpiew'  : 'libs/piewpiew/piewpiew-0.0.1.min',
+
+        // Require JS plugins
+        'text'      : 'libs/requirejs/text',
+        'order'     : 'libs/requirejs/order',
+        'i18n'      : 'libs/requirejs/i18n',
+        'domReady'  : 'libs/requirejs/domReady'
+      }
+    };
+  }
+
+  require.config(requireConfig);
 
   require(['piewpiew'], function(piewpiew) 
   {
--- examples/requirejs/js/config/config.js
+++ examples/requirejs/js/config/config.js
@@ -2,35 +2,33 @@ define(function() {
 	return {
 		version: "0.0.1",
 
-    environment: "development",
+        environment: "development",        
 
-    requirejs: {
-      paths: {
-        // Common libraries
-        'amd-loader': 'libs/amd-loader', // Wrapper for loading non-amd libs
-        'jquery'    : 'libs/jquery/jquery-1.7.1.min',
-        'underscore': 'libs/underscore/underscore',
-        'backbone'  : 'libs/backbone/backbone',
-        'piewpiew'  : 'libs/piewpiew/piewpiew-0.0.1.min',
+        development: {
 
-        // Require JS plugins
-        'text'      : 'libs/requirejs/text',
-        'order'     : 'libs/requirejs/order',
-        'i18n'      : 'libs/requirejs/i18n',
-        'domReady'  : 'libs/requirejs/domReady'
-      }
-    },
+        },
 
-    development: {
+        test: {
 
-    },
+        },
 
-    test: {
+        production: {
+          requirejs: {
+            paths: {
+              // Common libraries
+              'amd-loader': 'libs/amd-loader', // Wrapper for loading non-amd libs
+              'jquery'    : 'libs/jquery/jquery-1.7.1.min',
+              'underscore': 'libs/underscore/underscore',
+              'backbone'  : 'libs/backbone/backbone',
+              'piewpiew'  : 'libs/piewpiew/piewpiew-0.0.1.min',
 
-    },
-
-    production: {
-      
-    }
+              // Require JS plugins
+              'text'      : 'libs/requirejs/text',
+              'order'     : 'libs/requirejs/order',
+              'i18n'      : 'libs/requirejs/i18n',
+              'domReady'  : 'libs/requirejs/domReady'
+            }
+          }
+        }
 	}
 });
